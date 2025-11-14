aa51024900ca0461e98371a1bcb5543687e271e6
Bluemoon105 <lyj69641@gmail.com>
2025-11-14 09:11:33 +0900

    change type

--- src/main/java/com/app/medibear/controller/SleepChatController.java
+++ src/main/java/com/app/medibear/controller/SleepChatController.java
@@ -1,24 +1,31 @@
 package com.app.medibear.controller;
 
 import com.app.medibear.service.SleepLLMService;
+import com.app.medibear.utils.GetMemberId;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/chat")
 public class SleepChatController {
 
     private final SleepLLMService llmService;
+    private final GetMemberId getMemberId;
 
-    public SleepChatController(SleepLLMService llmService) {
-        this.llmService = llmService;
-    }
+//    public SleepChatController(SleepLLMService llmService) {
+//        this.llmService = llmService;
+//    }
 
     /** 일반 대화 */
     @PostMapping("/message")
-    public ResponseEntity<Map<String, Object>> chat(@RequestBody Map<String, Object> body) {
+    public ResponseEntity<Map<String, Object>> chat(@RequestBody Map<String, Object> body, HttpServletRequest request) {
 
         String email = body.get("email").toString();
         String message = body.get("message").toString();
@@ -29,22 +36,28 @@ public class SleepChatController {
 
     /** 일간 리포트 */
     @GetMapping("/report/daily/{email}")
-    public ResponseEntity<Map<String, Object>> dailyReport(@PathVariable String email) {
-        String report = llmService.getDailyReport(email);
+    public ResponseEntity<Map<String, Object>> dailyReport(@PathVariable String email, HttpServletRequest request) {
+    	String authorizationHeader = request.getHeader("Authorization");
+        String memberId =  getMemberId.getMemberId(authorizationHeader);
+    	String report = llmService.getDailyReport(email);
         return ResponseEntity.ok(Map.of("report", report));
     }
 
     /** 주간 리포트 */
     @GetMapping("/report/weekly/{email}")
-    public ResponseEntity<Map<String, Object>> weeklyReport(@PathVariable String email) {
-        String report = llmService.getWeeklyReport(email);
+    public ResponseEntity<Map<String, Object>> weeklyReport(@PathVariable String email, HttpServletRequest request) {
+    	String authorizationHeader = request.getHeader("Authorization");
+        String memberId =  getMemberId.getMemberId(authorizationHeader);
+    	String report = llmService.getWeeklyReport(email);
         return ResponseEntity.ok(Map.of("report", report));
     }
 
     /** 대화 기록 조회 */
     @GetMapping("/history/{email}")
-    public ResponseEntity<Map<String, Object>> history(@PathVariable String email) {
-        Map<String, Object> history = llmService.getChatHistory(email);
+    public ResponseEntity<Map<String, Object>> history(@PathVariable String email, HttpServletRequest request) {
+    	String authorizationHeader = request.getHeader("Authorization");
+        String memberId =  getMemberId.getMemberId(authorizationHeader);
+    	Map<String, Object> history = llmService.getChatHistory(email);
         return ResponseEntity.ok(history);
     }
 }
--- src/test/java/com/app/medibear/MedibearApplicationTests.java
+++ src/test/java/com/app/medibear/MedibearApplicationTests.java
@@ -1,24 +1,24 @@
-package com.app.medibear;
-
-import com.app.medibear.fastapi.service.impl.FastapiServiceImpl;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MedibearApplicationTests {
-
-  @Autowired
-  private FastapiServiceImpl fastapiServiceImpl;
-
-  @Test
-  void contextLoads() {
-  }
-
-  @Test
-  public void testFastapiPrediction() {
-    String predict = fastapiServiceImpl.getPrediction();
-    System.out.println(predict);
-  }
-
-}
+//package com.app.medibear;
+//
+//import com.app.medibear.fastapi.service.impl.FastapiServiceImpl;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MedibearApplicationTests {
+//
+//  @Autowired
+//  private FastapiServiceImpl fastapiServiceImpl;
+//
+//  @Test
+//  void contextLoads() {
+//  }
+//
+//  @Test
+//  public void testFastapiPrediction() {
+//    String predict = fastapiServiceImpl.getPrediction();
+//    System.out.println(predict);
+//  }
+//
+//}
