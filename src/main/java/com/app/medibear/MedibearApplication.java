8be4b96df5140f3ce2853dba53c68d89a891abfd
test <test@gimjeong-gyus-MacBook-Pro.local>
2025-11-20 17:15:16 +0900

    sdfsdf

--- src/main/java/com/app/medibear/MedibearApplication.java
+++ src/main/java/com/app/medibear/MedibearApplication.java
@@ -2,18 +2,14 @@ package com.app.medibear;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 @SpringBootApplication
 public class MedibearApplication {
 
   public static void main(String[] args) {
 
-    String raw = "1234";
 
-
-    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-    System.out.println("matches = " + encoder.encode(raw));
 
     SpringApplication.run(MedibearApplication.class, args);
   }
